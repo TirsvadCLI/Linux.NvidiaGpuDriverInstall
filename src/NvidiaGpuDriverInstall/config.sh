f82f41030c182d14b6a1d21a3f14a37c4afc5a4e
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2023-03-04 17:28:18 +0100

    Driver ver 64-525.89.02

--- src/NvidiaGpuDriverInstall/config.sh
+++ src/NvidiaGpuDriverInstall/config.sh
@@ -1,7 +1,7 @@
 # Uncomment the driver you. As default we load the newest!
-DRIVERLINK=https://us.download.nvidia.com/XFree86/Linux-x86_64/515.65.01/NVIDIA-Linux-x86_64-515.65.01.run
-
+DRIVERLINK=https://us.download.nvidia.com/XFree86/Linux-x86_64/525.89.02/NVIDIA-Linux-x86_64-525.89.02.run
+#DRIVERLINK=https://us.download.nvidia.com/XFree86/Linux-x86_64/515.65.01/NVIDIA-Linux-x86_64-515.65.01.run
 #DRIVERLINK=https://us.download.nvidia.com/XFree86/Linux-x86_64/470.141.03/NVIDIA-Linux-x86_64-470.141.03.run
 
-# CHANGE ME
-PASSWORD='Secret1234'
+# UEFI password
+PASSWORD='Secret1234' # CHANGE ME
